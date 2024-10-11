35928bd0c35fd876a74fc2cdea0b1901ac0ec6d4
marvin <sea-son@foxmail.com>
2024-10-11 18:28:03 +0800

    vite babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from '@babel/core';
+import parser from '@babel/parser';
+import traverse from '@babel/traverse';
+import generate from '@babel/generator';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        {
+            name: 'i18n-plugin',
+            configResolved() {
+                console.log('i18n-plugin loaded');
+            },
+            transform(code, id) {
+                // 过滤掉非 JavaScript/TypeScript 文件
+                if (!id.match(/\.(tsx)$/)) return;
+                console.log('id', id)
+                // 使用 Babel parser 解析代码成 AST
+                const ast = parser.parse(code, {
+                    sourceType: 'module',
+                    plugins: ['jsx'],
+                });
+
+                traverse.default(ast, {
+                    StringLiteral(path) {
+                        path.skip();
+                    },
+                });
+
+                // 生成新的代码
+                const output = generate.default(ast, {}, code);
+
+                return {
+                    code: output.code,
+                    map: output.map,
+                };
+            }
+        },
+        react()
+    ],
 })
