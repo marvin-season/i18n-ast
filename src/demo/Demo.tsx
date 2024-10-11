b29c593b2b8fcf8826e70b02c85f62567d838f52
marvin <sea-son@foxmail.com>
2024-10-11 11:32:09 +0800

    readme

--- src/demo/Demo.tsx
+++ src/demo/Demo.tsx
@@ -1,10 +1,9 @@
-import { t } from 'i18next';
 const menuData = ['驾驶舱'];
 const hasMenu = (name: string) => {
   return !!menuData.find((item: any) => item === name);
 };
 export const Demo = () => {
   return <>
-        {menuData.find(item => ['驾驶舱', '数据集'].includes(item)) && hasMenu('驾驶舱') && <div data-nav={t('ask-and-learn.IeTBGXx_Mlf-L4YdX02hM')}>{t('ask-and-learn.k19hPwg4bzcFkkpehbZv5')}</div>}
+        {menuData.find(item => ['驾驶舱', '数据集'].includes(item)) && hasMenu('驾驶舱') && <div data-nav={'驾驶舱'}>{'驾驶舱'}</div>}
     </>;
 };
