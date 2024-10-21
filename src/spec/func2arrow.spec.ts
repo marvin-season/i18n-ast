f5fa5649743ca7d6039b8419017776e47d02714c
marvin <sea-son@foxmail.com>
2024-10-21 18:13:29 +0800

    func2arrow

--- src/spec/func2arrow.spec.ts
+++ src/spec/func2arrow.spec.ts
@@ -1,4 +1,33 @@
 import { test } from "vitest";
+import parser from "@babel/parser";
+import babelTraverse from "@babel/traverse";
+import babelGenerate from "@babel/generator";
+import types from "@babel/types";
+
+const code = `
+function getName() {
+  return "hello";
+}
+`;
 
 test("func2arrow", async () => {
+  const ast = parser.parse(code, { plugins: [] });
+
+  babelTraverse(ast, {
+    FunctionDeclaration(path) {
+      const { node } = path;
+      console.log(node.type);
+      path.replaceWith(
+        types.variableDeclaration("const", [
+          types.variableDeclarator(
+            types.identifier(node.id?.name || 'aaa'), // variable 'a'
+            types.arrowFunctionExpression(node.params, node.body, node.async), // arrow function
+          ),
+        ]),
+      );
+    },
+  });
+
+  const output = babelGenerate(ast);
+  console.log(output.code);
 });
