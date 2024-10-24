6445a3e3fe8a1c6b8010845364951ee426f4e73c
marvin <sea-son@foxmail.com>
2024-10-24 14:08:38 +0800

    整理优化ast_empty__code

---
+++ src/spec/clean.spec.ts
@@ -0,0 +1,27 @@
+import { test } from "vitest";
+import parser from "@babel/parser";
+import babelTraverse from "@babel/traverse";
+import babelGenerate from "@babel/generator";
+import types from "@babel/types";
+import traverse from "@babel/traverse";
+import generate from "@babel/generator";
+
+const code = `
+function getName() {
+  return "hello";
+}
+`;
+
+test("clean_code", () => {
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["jsx", "typescript"],
+  });
+  traverse(ast, {
+
+  })
+
+  const output = generate(ast, { jsescOption: { minimal: true } });
+
+  console.log('output code\n', output.code);
+})
