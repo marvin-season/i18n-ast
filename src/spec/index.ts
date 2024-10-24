38953cc3926b17f6ee58e63a07d5aa0d46b90008
marvin <sea-son@foxmail.com>
2024-10-24 14:06:09 +0800

    整理优化

--- src/spec/index.ts
+++
@@ -1,6 +0,0 @@
-import parser from "@babel/parser";
-import babelGenerate from "@babel/generator";
-
-const ast = parser.parse('const a = 1;');
-
-console.log(babelGenerate(ast).code);
--- src/spec/index2.js
+++
@@ -1,6 +0,0 @@
-import parser from "@babel/parser";
-import babelGenerate from "@babel/generator";
-
-const ast = parser.parse('');
-
-console.log(babelGenerate(ast).code);
