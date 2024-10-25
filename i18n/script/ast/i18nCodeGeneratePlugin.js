5a5a33eb5d38f57a948c200bec56b7ed65346c35
marvin <sea-son@foxmail.com>
2024-10-25 13:42:04 +0800

    fix: clean workspace of i18n

--- i18n/script/ast/i18nCodeGeneratePlugin.js
+++
@@ -1,9 +0,0 @@
-import generator from "@babel/generator";
-
-const generate = generator.default;
-
-export const i18nCodeGeneratePlugin = {
-  run({ ast, config }) {
-    config.transformed = generate(ast);
-  },
-};
