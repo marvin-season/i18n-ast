3defde5436e27984ab452b1e7eb6ec942d0e27a6
marvin <sea-son@foxmail.com>
2024-10-25 14:49:43 +0800

    fix: target: "esnext",

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { i18nPlugin } from "./plugins/i18nPlugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [i18nPlugin(), react()],
+  build: {
+    target: "esnext",
+  }
 });
